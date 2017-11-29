06690780f1bf46ae3fa79b8f04f904272fc846ae
Alaa <alaulwan@hotmail.com>
2017-11-29 20:39:22 +0100

    REFACTOR

--- assign1_TDD/src/Calculator/model/AdvancedCalculator.java
+++ assign1_TDD/src/Calculator/model/AdvancedCalculator.java
@@ -2,9 +2,9 @@ package Calculator.model;
 
 public class AdvancedCalculator {
 
-	public double squareRoot (double A)  {
-		if (A>=0) {
-			return Math.sqrt(A);
+	public double squareRoot(double positiveNumber) {
+		if (positiveNumber >= 0) {
+			return Math.sqrt(positiveNumber);
 		}
 		throw new IllegalArgumentException("Can not Square Root a negative number");
 	}
