62867878dcef5200446d699e8db0d024062eafdd
Alaa <alaulwan@hotmail.com>
2017-11-30 04:27:48 +0100

    GREEN

--- assign1_TDD/src/Calculator/model/EquationDiscriminantCalculator.java
+++ assign1_TDD/src/Calculator/model/EquationDiscriminantCalculator.java
@@ -1,13 +1,16 @@
 package Calculator.model;
 
 public class EquationDiscriminantCalculator {
-
+	private Equation equation;
 
 	public EquationDiscriminantCalculator(Equation equation) {
-
+		this.equation = equation;
 	}
 
 	public double getDiscriminant() {
-		return 0;
+		StandardCalculator SC = new StandardCalculator();
+		AdvancedCalculator AC = new AdvancedCalculator();
+		double A_mul_C = SC.mul(this.equation.getA(), this.equation.getC());
+		return AC.power(this.equation.getB(), 2) - SC.mul(4, A_mul_C);
 	}
 }
