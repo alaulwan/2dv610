68f60b3ad7282c2b24dca9dc049dea5c97b7d86e
Alaa <alaulwan@hotmail.com>
2017-12-01 21:13:00 +0100

    REFACTOR

--- assign1_TDD/src/Calculator/model/FirstDegreeEquationCalculator.java
+++ assign1_TDD/src/Calculator/model/FirstDegreeEquationCalculator.java
@@ -2,18 +2,20 @@ package Calculator.model;
 
 public class FirstDegreeEquationCalculator {
 
-	private Equation equation;
+	private double a, b, c;
 	private double Solution;
 
 	public FirstDegreeEquationCalculator(Equation equation) {
 
-		this.equation = equation;
+		a = equation.getA();
+		b = equation.getB();
+		c = equation.getC();
 
 	}
 
 	private void startSolving() {
 		StandardCalculator SC = new StandardCalculator();
-		Solution = SC.divide(-equation.getC(), equation.getB());
+		Solution = SC.divide(-c, b);
 	}
 
 	public double getSolution() {
