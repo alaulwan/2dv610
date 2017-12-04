49b95a593d41ff1cd6836e919099a12b75cdb85f
Alaa <alaulwan@hotmail.com>
2017-12-04 21:16:06 +0100

    GREEN

--- assign1_TDD/src/Calculator/View/EnglishView.java
+++ assign1_TDD/src/Calculator/View/EnglishView.java
@@ -3,13 +3,19 @@ package Calculator.View;
 import java.io.PrintStream;
 
 public class EnglishView {
-	
+	private PrintStream printer;
 	
 	public EnglishView(PrintStream p) {
-		
+		this.printer = p;
 	}
 	
 	public void instructionPrint() {
-		
+		printer.println("Welcome to calculator");
+		printer.println("Enter:\n"
+				+ "s: standard calculator\n"
+				+ "d: advanced calculator\n"
+				+ "f: First Degree Equation Calculator a.x + b = 0\n"
+				+ "q: Quadratic Equatio Calculator a.x^2 + b.x + c = 0\n"
+				+ "e: exit");
 	}
 }
