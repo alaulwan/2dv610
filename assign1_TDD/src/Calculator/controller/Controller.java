2a729c601ee60f3b5e58573fd66bd78cc7215d09
Alaa <alaulwan@hotmail.com>
2017-12-06 01:53:29 +0100

    GREEN

--- assign1_TDD/src/Calculator/controller/Controller.java
+++ assign1_TDD/src/Calculator/controller/Controller.java
@@ -16,8 +16,48 @@ public class Controller {
 	}
 	
 	public double standardCalculator() {
-		return 0;
+		view.standardInstructionDisplay();
+		double result = Double.MIN_VALUE;
+		double number1 = Double.MIN_VALUE;
+		double number2 = Double.MIN_VALUE;
+		char operation = Character.MIN_VALUE;
 		
+		
+		number1 = getNumberFromUser(1);
+		if (number1== Double.MAX_VALUE)
+			return Double.MIN_VALUE;
+
+		operation = getOperationFromUser(Arrays.asList ('+','-','*','/','%','c'), false);
+		if (operation== 'c')
+			return Double.MIN_VALUE;
+
+		number2 = getNumberFromUser(2);
+		if (number2== Double.MAX_VALUE)
+			return Double.MIN_VALUE;
+
+		switch (operation) {
+		case '+': result = sc.sum(number1, number2);
+			break;
+		case '-': result = sc.subtract(number1, number2);
+			break;
+		case '*': result = sc.mul(number1, number2);
+			break;
+		case '/': try {
+				result = sc.divide(number1, number2);
+			} catch (IllegalArgumentException e) {
+				view.printText("Error*****"+ e.getMessage()+"*****\n");
+				return Double.MIN_VALUE;
+			}
+			break;
+		case '%': try {
+				result = sc.mod(number1, number2);
+			} catch (IllegalArgumentException e) {
+				view.printText(e.getMessage()+"\n");
+				return Double.MIN_VALUE;
+			}
+			break;
+		}
+		return result;
 	}
 
 	public double getNumberFromUser(int i) {
